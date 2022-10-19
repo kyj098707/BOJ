3af6f7256b9d1bb187da10d3bb4c64947485c6c7
kyj098707 <kyj098707@naver.com>
2022-10-19 23:10:17 +0900

    add : 2557, 2752, 9498, 10171, 10869, 10871

---
+++ basic/10871.py
@@ -0,0 +1,10 @@
+_, X = map(int, input().split())
+
+input_list = list(map(int,input().split()))
+
+for num in input_list:
+    
+    if X > num:
+        print(num, end= ' ')
+
+    
---
+++ basic/2752.py
@@ -0,0 +1,4 @@
+input_list = list(map(int, input().split()))
+
+for i in sorted(input_list):
+    print(i, end=' ')
