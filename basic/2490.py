3d76bea2ff9ac32653da7b5dc1d640e1c38af819
kyj098707 <kyj098707@naver.com>
2022-11-02 11:29:43 +0900

    add : 2490

---
+++ basic/2490.py
@@ -0,0 +1,14 @@
+for _ in range(3):
+    tmp = list(map(int, input().split()))
+    if tmp.count(1) == 3:
+        print('A')
+    elif tmp.count(1) == 2:
+        print('B')
+    elif tmp.count(1) == 1:
+        print('C')
+    elif tmp.count(1) == 0:
+        print('D')
+    else:
+        print('E')
+
+ 
