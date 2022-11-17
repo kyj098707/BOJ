a07633ab8734c1d7842afe0aeb8c48a90667aa20
kyj098707 <kyj098707@naver.com>
2022-11-17 20:08:12 +0900

    22.11.17

---
+++ backtracking/15657.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+
+num_list = sorted(list(map(int, input().split())))
+result_list = [0] * (n+1)
+
+def func(k):
+    if k == m:
+        for i in range(m):
+            print(result_list[i], end = ' ')
+        print()
+        return
+
+    for i in range(1,n+1):
+        if i != 0 and result_list[k-1] > num_list[i-1]:
+            continue 
+        result_list[k] = num_list[i-1]
+        func(k+1)
+
+func(0)
+
+        
+    
