095b30880dd0138d195cc7985870658114e67092
kyj098707 <kyj098707@naver.com>
2022-11-19 18:28:33 +0900

    22.11.19

---
+++ backtracking/15665.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+result = [0] * (m+1)
+tmp_list = sorted(list(map(int, input().split())))
+num_list = []
+tmp = 0
+for num in tmp_list:
+    if tmp == num:
+        continue
+    tmp = num
+    num_list.append(num)
+
+def func(k):
+    if k == m:
+        for i in range(m):
+            print(result[i],end = " ")
+        print()
+        return
+    for i in range(len(num_list)):
+        result[k] = num_list[i]
+        func(k+1)
+
+func(0)
--- recursion/11729.py
+++ recursion/11729.py
@@ -1,6 +1,4 @@
 # n개의 하노이가 있을 때 n-1개를 2에, n의 하노이를 3에, 2에서 3으로 가는 n-1를 한번 더
-
-
 def hanoi(st, en, n):
     if n == 1:
         print(st, en)
