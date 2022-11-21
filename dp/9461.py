f036251a5162dd9a23bb83edc876762152e46077
kyj098707 <kyj098707@naver.com>
2022-11-21 17:27:43 +0900

    22.11.21

---
+++ dp/9461.py
@@ -0,0 +1,13 @@
+D = [0] * 103
+D[1] = 1
+D[2] = 1
+D[3] = 1
+D[4] = 2
+D[5] = 2
+
+for i in range(6,101):
+    D[i] = D[i-1] + D[i-5]
+
+n = int(input())
+for _ in range(n):
+    print(D[int(input())])
