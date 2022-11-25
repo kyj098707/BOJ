0f167032ce57f9a4e5fab67b64862988a1c94029
kyj098707 <kyj098707@naver.com>
2022-11-25 11:02:37 +0900

    22.11.25

---
+++ boj_roadmap_1/2751.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+#python 정렬은 n logn
+n = int(input())
+num_list = []
+for _ in range(n):
+    num_list.append(int(input()))
+
+for num in sorted(num_list):
+    print(num)
